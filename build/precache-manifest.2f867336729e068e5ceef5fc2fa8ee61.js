1a7554edcf7a73d6b3a31986ae2d825bb6e892df
skeletormentor <50324900+skeletormentor@users.noreply.github.com>
2020-03-25 00:26:35 +0200

    update number added

---
+++ build/precache-manifest.2f867336729e068e5ceef5fc2fa8ee61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb4e1b686a325b79f744f13fe0369ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4991c4b98589e6a5dd3",
+    "url": "/static/css/main.d4cd494b.chunk.css"
+  },
+  {
+    "revision": "4da5ceadfe8e1b6dbc72",
+    "url": "/static/js/2.43cce481.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.43cce481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4991c4b98589e6a5dd3",
+    "url": "/static/js/main.8b666c66.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
